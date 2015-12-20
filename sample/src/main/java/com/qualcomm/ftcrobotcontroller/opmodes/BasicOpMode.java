e0e99339b175d7625510b484752143ad700224cc
Talor <ta.anders17@gmail.com>
2015-12-20 08:59:53 -0800

    blinkenlights, gyro

--- sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BasicOpMode.java
+++ sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BasicOpMode.java
@@ -1,15 +1,19 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
     import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+    import com.qualcomm.hardware.ModernRoboticsI2cGyro;
     import com.lasarobotics.library.controller.ButtonState;
     import com.lasarobotics.library.controller.Controller;
     import com.lasarobotics.library.drive.Tank;
     import com.qualcomm.robotcore.hardware.DcMotor;
+    import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+    import com.qualcomm.robotcore.hardware.LED;
     import com.qualcomm.robotcore.hardware.Servo;
 
 public class BasicOpMode extends OpMode {
 
     //Declare Servos, motors, and controllers
+    ModernRoboticsI2cGyro G;
     DcMotor frontLeft, frontRight, backLeft, backRight;
     Controller one;
     Controller two;
@@ -19,13 +23,23 @@ public class BasicOpMode extends OpMode {
     Servo Rservo;
 
     //declare Reverse Variable
-    public double reverse = 1;
+    public double reverse = -1;
 
     double Lservopos;
     double Rservopos;
+    double heading;
+
+    DeviceInterfaceModule cdim;
+    LED light;
+
+    public BasicOpMode() {
+    }
 
     @Override
     public void init() {
+        //DO NOT MOVE ROBOT DURING INIT OR GYRO WONT CALIBRATE
+        G = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
+        G.calibrate();
         //assign variables to hardware from config
         frontLeft = hardwareMap.dcMotor.get("4");
         frontRight = hardwareMap.dcMotor.get("2");
@@ -35,6 +49,9 @@ public class BasicOpMode extends OpMode {
         arm2 = hardwareMap.dcMotor.get("6");
         Lservo = hardwareMap.servo.get("s2"); // channel 6
         Rservo = hardwareMap.servo.get("s1"); // channel 1
+
+        cdim = hardwareMap.deviceInterfaceModule.get("dim");
+        light = new LED(cdim, 0);
 
         one = new Controller(gamepad1);
         two = new Controller(gamepad2);
@@ -74,24 +91,42 @@ public class BasicOpMode extends OpMode {
         //right- down = 0.
         //servo is pulled down by pressing a button, and goes back up when released.
         if (two.x == ButtonState.HELD) Lservopos = 1; else Lservopos = 0;
-        if (two.a == ButtonState.HELD) Rservopos = 0; else Rservopos = 1;
+        if (two.b == ButtonState.HELD) Rservopos = 0; else Rservopos = 1;
 
         Lservo.setPosition(Lservopos);
         Rservo.setPosition(Rservopos);
         //--------------------------------DIRECTION
-        //This reverses our robot, wo what was once our back is now our front.
+        //This reverses our robot, so what was once our back is now our front.
         if (one.x == ButtonState.RELEASED) reverse = -reverse;
 
-        //'left' and 'right' variables correspond to our left and right side motor values since we have 4wd.
-        left  = reverse * one.left_stick_y;
-        right = reverse * one.right_stick_y;
+        if (reverse == 1) {
+            left  = one.left_stick_y;
+            right = one.right_stick_y;
+            light.enable(true);
+        } else {
+            left  = -one.right_stick_y;
+            right = -one.left_stick_y;
+            light.enable(false);
+        }
+        //--------------------------------GYRO
+        heading = G.getIntegratedZValue();
+        if (one.b == ButtonState.RELEASED) G.resetZAxisIntegrator();
+        if (one.back == ButtonState.RELEASED) G.calibrate();
 
+        //--------------------------------DRIVE
         //uses FTClib Tank class. Thanks LASA!
         Tank.motor4(frontLeft, frontRight, backLeft, backRight, -left, right);
 
         //set arm motor power
         arm2.setPower(tape);
         arm1.setPower(winch);
+
+
+        //--------------------------------TELEMETRY
+        telemetry.addData("direction",reverse);
+        telemetry.addData("Heading", heading);
+
+
     }
 
     public void stop() {
--- sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -39,13 +39,13 @@ import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
  */
 public class FtcOpModeRegister implements OpModeRegister {
 
-  /**
-   * The Op Mode Manager will call this method when it wants a list of all
-   * available op modes. Add your op mode to the list to enable it.
-   *
-   * @param manager op mode manager
-   */
-  public void register(OpModeManager manager) {
+    /**
+    * The Op Mode Manager will call this method when it wants a list of all
+    * available op modes. Add your op mode to the list to enable it.
+    *
+    * @param manager op mode manager
+    */
+public void register(OpModeManager manager) {
 
     /*
      * register your op modes here.
@@ -56,44 +56,12 @@ public class FtcOpModeRegister implements OpModeRegister {
      */
 
     manager.register("NullOp", NullOp.class);
-
-    //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-
     manager.register("BasicOpMode", BasicOpMode.class);
-    manager.register("ServoCode", ServoCode.class);
+    //manager.register("ServoCode", ServoCode.class);
     manager.register("Auton", Auton.class);
+    manager.register("MRGyroExample", MRGyroExample.class);
+    //manager.register("GyroAuton", GyroAuton.class);
+    manager.register("DumpOp", dumpOp.class);
 
-
-    /*
-     * Uncomment any of the following lines if you want to register an op mode.
-     */
-    //manager.register("MR Gyro Test", MRGyroTest.class);
-
-    //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
-    //manager.register("ColorSensorDriver", ColorSensorDriver.class);
-
-    //manager.register("IrSeekerOp", IrSeekerOp.class);
-    //manager.register("CompassCalibration", CompassCalibration.class);
-    //manager.register("I2cAddressChangeExample", LinearI2cAddressChange.class);
-
-
-    //manager.register("NxtTeleOp", NxtTeleOp.class);
-
-    //manager.register("LinearK9TeleOp", LinearK9TeleOp.class);
-    //manager.register("LinearIrExample", LinearIrExample.class);
-
-
-    //manager.register ("PushBotManual1", PushBotManual1.class);
-    //manager.register ("PushBotAutoSensors", PushBotAutoSensors.class);
-    //manager.register ("PushBotIrEvent", PushBotIrEvent.class);
-
-    //manager.register ("PushBotManualSensors", PushBotManualSensors.class);
-    //manager.register ("PushBotOdsDetectEvent", PushBotOdsDetectEvent.class);
-    //manager.register ("PushBotOdsFollowEvent", PushBotOdsFollowEvent.class);
-    //manager.register ("PushBotTouchEvent", PushBotTouchEvent.class);
-
-    //manager.register("PushBotDriveTouch", PushBotDriveTouch.java);
-    //manager.register("PushBotIrSeek", PushBotIrSeek.java);
-    //manager.register("PushBotSquare", PushBotSquare.java);
-  }
+}
 }
