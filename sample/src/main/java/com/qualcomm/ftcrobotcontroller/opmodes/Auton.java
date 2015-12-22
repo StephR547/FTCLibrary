21a32a020120e1c65e69c9fa55276a213325f7a0
Talor <ta.anders17@gmail.com>
2015-12-21 20:17:06 -0800

    moved setup to RobotSetup class

--- sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Auton.java
+++ sample/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Auton.java
@@ -1,10 +1,12 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.lasarobotics.library.drive.Tank;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-public class Auton extends RobotSetup {
+public class Auton extends OpMode{
     public long startTime;
     public long currentTime;
+    @Override public void init(){}
     @Override public void start() {
         startTime = System.currentTimeMillis();
     }
@@ -21,15 +23,13 @@ public class Auton extends RobotSetup {
         else {
             left = 0;
             right = 0;
-        }*/
+        }
         //uses FTCLib Tank class. Thanks LASA!
         Tank.motor4(frontLeft, frontRight, backLeft, backRight, -left, right);
         //send data to fones
         heading = G.getIntegratedZValue();
         telemetry.addData("direction", reverse);
-        telemetry.addData("Heading", heading);    }
+        telemetry.addData("Heading", heading);    */}
     @Override public void stop() {
-        left = 0;
-        right = 0;
     }
 }
