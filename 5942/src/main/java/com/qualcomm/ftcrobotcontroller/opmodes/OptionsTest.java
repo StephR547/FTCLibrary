c2f4f111a7b4b323490761a221c81b5c4cf88157
Talor <ta.anders17@gmail.com>
2015-12-23 13:34:15 -0800

    set up Options Menu with basic alliance switching.

--- 5942/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OptionsTest.java
+++ 5942/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OptionsTest.java
@@ -2,9 +2,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-/**
- * Created by ta on 12/23/15.
- */
+
 public class OptionsTest extends LinearOpMode{
     @Override public void runOpMode() throws InterruptedException{
         RobotSetup bot  = new RobotSetup(hardwareMap,telemetry);
