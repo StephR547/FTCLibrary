468624f26356d05d7a82532502458c6dc54c031d
Talor <ta.anders17@gmail.com>
2015-12-23 15:39:36 -0800

    alliance select in LinearAuton

--- 5942/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/linearAuton.java
+++ 5942/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/linearAuton.java
@@ -3,27 +3,22 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.lasarobotics.library.options.OptionMenu;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-/**
- * Created by ta on 12/21/15.
- */
 public class linearAuton extends LinearOpMode{
     @Override
     public void runOpMode() throws InterruptedException {
         RobotSetup bot = new RobotSetup(hardwareMap,telemetry);
-        bot.redLED(true);
         bot.startRobot();
         bot.G.calibrate();
-
+        bot.allianceMenu.show();
+        while (bot.getAlliance().equals("None")){
+            sleep(50);
+        }
+        String alliance = bot.getAlliance();
+        bot.redLED(bot.isRed());
+        bot.blueLED(bot.isBlue());
+        telemetry.addData("Alliance", alliance);
         //--------------------------------OPMODE START
         waitForStart();
-        bot.redLED(false);
-
-        bot.encoderMove(1600, 0.3);
-        sleep(100);
-        bot.gTurn(-90, 0.1);
-        sleep(250);
-        telemetry.addData("Gyro",bot.heading());
-        bot.end();
 
     }
 }
