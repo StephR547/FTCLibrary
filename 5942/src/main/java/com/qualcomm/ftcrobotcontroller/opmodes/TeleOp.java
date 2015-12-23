3ea8fdb56d74f3b2518bdc6a70ffd8596d32148c
Talor <ta.anders17@gmail.com>
2015-12-22 17:18:43 -0800

    jarjarbling

--- 5942/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
+++ 5942/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
@@ -9,11 +9,11 @@ public class TeleOp extends OpMode {
     //--------------------------------CONTROLLER SETUP
     Controller  one = new Controller(gamepad1),
                 two = new Controller(gamepad2);
-    RobotSetup  jarjar;
+    RobotSetup  jarjarbling;
 
     @Override public void init(){
-        jarjar = new RobotSetup(hardwareMap, telemetry);
-        jarjar.gyroInit();//TODO light doesnt seem to work. needs more testing.
+        jarjarbling = new RobotSetup(hardwareMap, telemetry);
+        jarjarbling.gyroInit();//TODO light doesnt seem to work. needs more testing.
 
     }
     @Override public void loop() {
@@ -24,36 +24,36 @@ public class TeleOp extends OpMode {
 
         //--------------------------------ARM
         //Controls tape with left bumpers. If bumpers are not pressed, use the joystick value.
-        if      (two.left_trigger == 1) jarjar.moveTape(0.2);
-        else if (two.left_bumper  == ButtonState.HELD) jarjar.moveTape(-0.2);
-        else    jarjar.moveTape(gamepad2.left_stick_y);
+        if      (two.left_trigger == 1) jarjarbling.moveTape(0.2);
+        else if (two.left_bumper  == ButtonState.HELD) jarjarbling.moveTape(-0.2);
+        else    jarjarbling.moveTape(gamepad2.left_stick_y);
 
         //controls winch with right buttons. we don't use a joystick here.
-        if      (two.right_trigger == 1) jarjar.moveWinch(1);
-        else if (two.right_bumper == ButtonState.HELD) jarjar.moveWinch(-1);
-        else    jarjar.moveWinch(0);;
+        if      (two.right_trigger == 1) jarjarbling.moveWinch(1);
+        else if (two.right_bumper == ButtonState.HELD) jarjarbling.moveWinch(-1);
+        else    jarjarbling.moveWinch(0);;
 
         //--------------------------------SERVOS
         //left  down = 1.
         //right down = 0.
         //servo is pulled down by holding a button, and goes back up when released.
-        if (two.x == ButtonState.HELD) jarjar.servoL(1); else jarjar.servoL(0);
-        if (two.b == ButtonState.HELD) jarjar.servoR(0); else jarjar.servoR(1);
+        if (two.x == ButtonState.HELD) jarjarbling.servoL(1); else jarjarbling.servoL(0);
+        if (two.b == ButtonState.HELD) jarjarbling.servoR(0); else jarjarbling.servoR(1);
 
         //--------------------------------DIRECTION
         //This reverses our robot, so what was once our back is now our front.
         //call reverse() to switch directions, and check reverseVal for current state.
-        if (one.x == ButtonState.RELEASED) jarjar.reverse();
-        jarjar.redLED(jarjar.isreversed());
+        if (one.x == ButtonState.RELEASED) jarjarbling.reverse();
+        jarjarbling.redLED(jarjarbling.isreversed());
 
-        if (jarjar.isreversed()) { //assigns left  to right and flips directions.
-            jarjar.move(-one.right_stick_y, -one.left_stick_y);  //TODO CHECK THIS '-' SIGN
+        if (jarjarbling.isreversed()) { //assigns left  to right and flips directions.
+            jarjarbling.move(-one.right_stick_y, -one.left_stick_y);  //TODO CHECK THIS '-' SIGN
         } else {
-            jarjar.move (one.left_stick_y, one.right_stick_y);
+            jarjarbling.move (one.left_stick_y, one.right_stick_y);
         }
-        jarjar.defaultTelemetry();
+        jarjarbling.defaultTelemetry();
     }
     public void stop(){
-        jarjar.end();
+        jarjarbling.end();
     }
 }
